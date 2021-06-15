2d2f9cd0921cd47c871e6638c4a933f02792039e
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-15 12:15:08 +0700

    Add project files.

---
+++ SK-Lib-cmath-frexp-Bagian6__CPP/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <limits>
+#include <conio.h>
+
+/*  Source by CPPReference
+    Modified For Learn by Rizky Khapidsyah
+    I.D.E : VS2019 */
+
+int main() {
+    double f = 123.45;
+    std::cout << "Given the number " << f << " or " << std::hexfloat << f << std::defaultfloat << " in hex,\n";
+
+    double f3;
+    double f2 = std::modf(f, &f3);
+    std::cout << "modf() makes " << f3 << " + " << f2 << '\n';
+
+    int i;
+    f2 = std::frexp(f, &i);
+    std::cout << "frexp() makes " << f2 << " * 2^" << i << '\n';
+
+    i = std::ilogb(f);
+    std::cout << "logb()/ilogb() make " << f / std::scalbn(1.0, i) << " * " << std::numeric_limits<double>::radix << "^" << std::ilogb(f) << '\n';
+
+    _getch();
+    return 0;
+}
